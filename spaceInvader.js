eb9a1c120ae65f9a77b513e6fff7f3d029750990
“Kobe <kobe.berckmans@student.ehb.be>
2022-10-09 19:27:41 +0200

    space invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,37 @@
 "use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+
+context.beginPath();
+ context.rect(50, 50, 300, 300);
+ context.fill();
+ context.stroke();
+ 
+
+ context.beginPath();
+ context.rect(100, 125, 200, 40);
+ context.fillStyle = 'lightgreen';
+ context.strokeStyle = 'lightgreen';
+ context.fill();
+ context.stroke();
+ 
+
+ context.beginPath();
+ context.rect(185, 100, 40, 200);
+ context.strokeStyle = 'lightgreen';
+ context.fillStyle = 'lightgreen';
+ context.fill();
+ context.stroke();
+
+ 
+ context.beginPath();
+ context.rect(150, 190, 110, 40);
+ context.strokeStyle = 'lightgreen';
+ context.fillStyle = 'lightgreen';
+ context.fill();
+ context.stroke();
+ 
+
